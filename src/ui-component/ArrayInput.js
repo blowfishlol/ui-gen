a549b811ba2f5390d2725666743353268fee9320
sake1 <timothydickys@gmail.com>
2018-03-12 13:47:50 +0100

    moti

---
+++ src/ui-component/ArrayInput.js
@@ -0,0 +1,38 @@
+import React from "react";
+import '@progress/kendo-theme-material/dist/all.css';
+
+import App from '../App';
+
+export default class ArrayInput extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemCount: 1
+    };
+  }
+
+  add() {
+    this.setState({
+      itemCount: (this.state.itemCount + 1)
+    });
+  }
+
+  render() {
+    var childKagebunshin = [];
+    for(var i = 0; i < this.state.itemCount; i++) {
+      childKagebunshin[i] = this.props.config.child_content;
+    }
+    var elements = childKagebunshin.map(element => {
+      return <App config={[this.props.config.child_content]} />
+    });
+    return(
+      <div>
+        <div>
+          {elements}
+        </div>
+        <button onClick={() => this.add()}>ADD</button>
+      </div>
+    )
+  }
+}
