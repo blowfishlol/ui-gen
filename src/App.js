f4b807472c4d6bee977d1a6ba91211046fb5f790
sake1 <timothydickys@gmail.com>
2018-03-12 11:37:08 +0100

    initial frame

--- src/App.js
+++ src/App.js
@@ -2,11 +2,61 @@ import React, { Component } from 'react';
 //import '@progress/kendo-theme-material/dist/all.css';
 import './App.css'
 
+import ComponentType from "./ComponentType"
+
 class App extends Component {
   render() {
+    var elements = this.props.config.map(element => {
+      if(element.type == ComponentType.TEXT) {
+        return <div>
+          text
+        </div>
+      } else if(element.type == ComponentType.NUMBER) {
+        return <div>
+          number
+        </div>
+      } else if(element.type == ComponentType.IMAGE) {
+        return <div>
+          image
+        </div>
+      } else if(element.type == ComponentType.DROPDOWN) {
+        return <div>
+          dropdown
+        </div>
+      } else if(element.type == ComponentType.CHECKBOX) {
+        return <div>
+          checkbox
+        </div>
+      } else if(element.type == ComponentType.TOGGLE) {
+        return <div>
+          toggle
+        </div>
+      } else if(element.type == ComponentType.DATE) {
+        return <div>
+          date
+        </div>
+      } else if(element.type == ComponentType.TIME) {
+        return <div>
+          time
+        </div>
+      } else if(element.type == ComponentType.ARRAY) {
+        return <div>
+          array
+        </div>
+      } else if(element.type == ComponentType.MAP) {
+        return <div>
+          map
+        </div>
+      } else {
+        return <div>
+          error
+        </div>
+      }
+    })
+
     return (
       <div>
-        Hello World!
+        {elements}
       </div>
     );
   }
