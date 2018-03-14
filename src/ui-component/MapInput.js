bdaff79ac69a7cbd68d7ff107d63c93469ea2c7a
Wibisono <wibisonoyulius@gmail.com>
2018-03-14 10:35:05 +0100

    cleanup on mapinput, not done yet

--- src/ui-component/MapInput.js
+++ src/ui-component/MapInput.js
@@ -17,7 +17,6 @@ export default class MapInput extends React.Component {
 
   add() {
       console.log("Ma Boi",this.state);
-      var newState = {...this.state};
     this.setState({
       ...this.state,
       childData: this.state.childData.concat(["uneeded data for now"])
@@ -33,7 +32,7 @@ export default class MapInput extends React.Component {
     const thisPath = this.state.path;
 
     //check if key has generated any child or not. if not, then the path of the parent must be appended to the child.
-    if(this.state.key==0){
+    if(this.state.key===0){
       //this.state.key++;
       const thisKey = this.state.key;
       this.props.config.child_content.forEach(function(child){
@@ -57,11 +56,7 @@ export default class MapInput extends React.Component {
       });
     }
     this.state.key++;
-    console.log(this.state.key,"KEYT");
-    /*for(let i = 0 ; i < this.props.config.child_content.length ; i++){
-      console.log(this.props.config.child_content[i].path, this.state.path);
-    //  this.props.config.child_content[i].path = this.state.path + "." + this.props.config.child_content[i].path;
-  }*/
+
     var elements = this.state.childData.map(element => {
       return <div class="mapChild">
         <App config={this.props.config.child_content} />
