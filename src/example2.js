9d50dd1f52af03bcf5d2e4a52389c3ec28d02caa
Wibisono <wibisonoyulius@gmail.com>
2018-03-15 16:25:11 +0100

    change on expample2.js and comment a little on evaluator.js

--- src/example2.js
+++ src/example2.js
@@ -19,7 +19,7 @@ var boi = [
                 "value": {
                     "default": false
                 },
-                "rendered": "installation.user.name==true"
+                "rendered": "installation.user.name == true"
             }
         ]
     },
