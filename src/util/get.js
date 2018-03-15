4c5208688b953800691eebff2dd2c4ec166980b2
sake1 <timothydickys@gmail.com>
2018-03-15 11:09:43 +0100

    get function done

--- src/util/get.js
+++ src/util/get.js
@@ -1,3 +1,48 @@
-export default function get(storage, path) {
-  return ""
+import storage from "./../storage"
+import ActionList from "./../reducer/actionList"
+
+export default function f(path, type) {
+  try {
+    return get(storage.getState().data, path.split("."), type)
+  } catch(error) {
+  }
+  return set(path, type)
+}
+
+function set(path, type) {
+  console.log("set new")
+  storage.dispatch({
+    type: ActionList.SET,
+    payload: {
+      "path": path,
+      "value": defaultValue(type)
+    }
+  })
+  return f(path, type)
+}
+
+function get(ptr, path, type) {
+  if(ptr[path[0]] === undefined) {
+    throw new Error()
+  }
+  if(path.length === 1) {
+    return ptr[path[0]]
+  }
+  return get(ptr[path[0]], path.slice(1), type)
+}
+
+function defaultValue(type) {
+  switch(type) {
+    case "text":     return ""
+    case "date":     return "2000-01-01"
+    case "image":    return ""
+    case "checkbox": return {}
+    case "time":     return "12:00"
+    case "toggle":   return false
+    case "dropdown": return ""
+    case "number":   return 0
+    case "array":    return []
+    case "map":      return {}
+    default:         return ""
+  }
 }
