71e2cd1dd3fdcb48fad0632db8a2ebcac931fc98
sake1 <timothydickys@gmail.com>
2018-03-15 13:10:55 +0100

    moti

--- src/reducer/actionList.js
+++ src/reducer/actionList.js
@@ -1,6 +1,8 @@
 var ActionList = {
-  SET:        "set_data",
-  SET_CONFIG: "set_config",
+  SET:         "set_data",
+  PUSH_STACK:  "push",
+  POP_STACK:   "pop",
+  SET_CONFIG:  "set_config",
 };
 
 export default(ActionList);
